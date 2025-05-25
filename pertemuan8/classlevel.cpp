3bb3f16532d97b65653be867ca25b4cd322f62e4
JefriAlBukhori200505 <jefrial@students.usu.ac.id>
2025-05-25 17:40:30 +0700

    pertemuan 8 : Struct & Class

--- pertemuan8/classlevel.cpp
+++ pertemuan8/classlevel.cpp
@@ -1,34 +1,34 @@
 #include<iostream>
 using namespace std;
 
-class contohakses{
-    private:
+class contohakses{ // deklarasi class utama
+    private: // hanya bisa diakses dari dalam class ini sendiri
         int privatevar;
 
-    protected:
+    protected: // bisa diakses dari dalam class ini dan class turunannya
         int protectedvar;
 
-    public:
+    public: // bisa diakses dari mana saja
         int publicvar;
         
-        
+        // constructor: untuk menginisialisasi nilai
         contohakses(){
             privatevar = 1;
             protectedvar = 2;
             publicvar = 3;
         }
 
-    void tampilkansemua() {
+    void tampilkansemua() { // untuk menampilkan semua variabel dari dalam class
         cout << " akses dari dalam class : " << endl;
         cout << privatevar << endl;
         cout << protectedvar << endl;
         cout << publicvar << endl;
     }
 };
-    // class turunan
+    // class turunan, class turunan dari contohakses
     class turunan : public contohakses{
         public:
-        void aksesprotected(){
+        void aksesprotected(){ // class turunan untuk mengakses variabel protected dan public
             cout << " akses publicvar " << publicvar << endl;
             cout << " akses protectedvar " << protectedvar << endl;
              
@@ -36,15 +36,15 @@ class contohakses{
     };
 
 int main() {
-    contohakses obj;
-    obj.tampilkansemua();
+    contohakses obj; // membuat objek dari class contohakses
+    obj.tampilkansemua(); // akses method public dari class
 
     cout << " akses dari luar class : " << endl;
     cout << obj.publicvar << endl;
     // cout << obj.privatevar << endl; // eror
     // cout << obj.protectedvar << endl; // eror
 
-    turunan tur ;
-    tur.aksesprotected();
+    turunan tur ; / membuat objek dari class turunan
+    tur.aksesprotected(); // bisa akses public dan protected, tapi bukan private
 
 }
