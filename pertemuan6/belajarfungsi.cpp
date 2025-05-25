b2e2b31557030ee8fd0115150b7f282784972d2b
JefriAlBukhori200505 <jefrial@students.usu.ac.id>
2025-05-25 16:57:33 +0700

    pertemuan 6 : Fungsi dan Header

--- pertemuan6/belajarfungsi.cpp
+++ pertemuan6/belajarfungsi.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 
-void sapa(string nama) {
+void sapa(string nama) { // fungsi bernama 'sapa' yang menerima parameter string 
     cout << "Halo " << nama << " ! Selamat Datang di AP 2 !" << endl;
 }
 
 int main () {
-    string namaPengguna = "Alya";
+    string namaPengguna = "Jefri"; // variabel namaPengguna dengan nilai "Jefri"
 
-    sapa(namaPengguna);
+    sapa(namaPengguna); // memanggil fungsi 'sapa' dengan mengirimkan 'namaPengguna' sebagai argumen
 
     return 0;
 }
