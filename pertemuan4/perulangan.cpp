be49af4f3ad31eacdc30a7c260cbe5b6162fd85c
JefriAlBukhori200505 <jefrial@students.usu.ac.id>
2025-05-25 16:17:58 +0700

    pertemuan 4 : perulangan

--- pertemuan4/perulangan.cpp
+++ pertemuan4/perulangan.cpp
@@ -7,7 +7,7 @@ int main() {
 
     // a:
     // cout << "hello word" << endl;
-    // goto d;
+    // goto d; // langsung loncat ke label d, melewati b dan c
     // b:
     // cout << "Fasilkom-TI" << endl;
     // c:
@@ -18,7 +18,8 @@ int main() {
     //menampilkan bilangan genap 10 -> 2 dengan goto
     //  
 
-    //statement while
+    //statement while, mengeksekusi kode selama kondisi bernilai true
+
     // int i = 1;
     // while (i <= 10){
     //     if (i % 2 == 0) {
@@ -32,13 +33,13 @@ int main() {
     //     cout << i << endl;
     // } while (i <= 0);
 
-    //statement for
+    //statement for, digunakan untuk perulangan yang diketahui batasnya
     //for (inisialisasi,kondisi,increase)
     // for (int i = 1; i <= 10; i+=2) {
     //     cout << "hello world" << endl;
     // }
 
-    //nested for
+    //nested for, digunakan untuk mencetak pola 2D, seperti bintang atau matriks
     //***** 5 X 5
     // for (int i = 1; i <= 5; i++) {
     //     for(int j = 1; j <= 5; j++){
@@ -47,7 +48,7 @@ int main() {
     //     cout << endl;
     // }
 
-    //segitiga siku-siku
+    //segitiga siku-siku, mencetak bintang dalam bentuk segitiga siku-siku
     // for(int i = 1; i <=5 ; i++){
     //     for (int j = 1; j <= i; j++){
     //         cout << "* ";
