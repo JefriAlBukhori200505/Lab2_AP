7aaf208382bfe4059fad315e93f2d29fd8808aff
JefriAlBukhori200505 <jefrial@students.usu.ac.id>
2025-05-25 14:41:31 +0700

    pertemuan 3 : operasi kondisi

--- pertemuan3/operasikondisi.cpp
+++ pertemuan3/operasikondisi.cpp
@@ -1,27 +1,27 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // header untuk cout/cin
+using namespace std; // agar tidak perlu menggunakan std:: sebelum cout/cin
 
 int main () {
-    int nilai;
+    int nilai; // variabel nilai bertipe integer
 
-    system("CLS");
+    system("CLS"); // membersihkan layar
 
     cout << "Masukkan nilai : ";
     cin >> nilai;
 
-    // if statement
+    // if statement, mengecek kondisi tertentu
     // if (nilai <= 65) {
     //     cout << "Anda tidak lulus!" << endl;
     // }
 
-    // if - else statement
+    // if - else statement, dua kemungkinan
     // if (nilai <= 65) {
     //     cout << "Anda tidak lulus!" << endl;
     // } else {
     //     cout << "Anda lulus !" << endl;
     // }
 
-    // if - else - if statement
+    // if - else - if statement, beberapa kondisi
     // if (nilai == 100) {
     //     cout << "Anda Keceh" << endl;
     // } else if (nilai <= 65) {
@@ -30,7 +30,7 @@ int main () {
     //     cout << "Anda lulus" << endl;
     // }
 
-    // nested if
+    // nested if, mengecek kondisi di dalam kondisi lainnya
     // if (nilai <= 65) {
     //     cout << "Anda tidak lulus" << endl;
     // } else {
@@ -41,11 +41,11 @@ int main () {
     //     }
     // }
 
-    // switch case
+    // switch case, untuk mengevaluasi satu variabel terhadap banyak nilai
     // switch (nilai) {
     //     case 1:
     //         cout << "Senin" << endl;
-    //         break;
+    //         break; // menghentikan eksekusi setelah satu case diproses
     //     case 2:
     //         cout << "Selasa" << endl;
     //         break;
@@ -69,7 +69,7 @@ int main () {
     //         break;
     // }
 
-    // switch range
+    // switch range, untuk menangani rentang nilai dalam pernyataan
     // switch (nilai) {
     //     case 85 ... 100 : cout << "A" << endl; break;
     //     case 80 ... 84 : cout << "B+" << endl; break;
@@ -80,7 +80,7 @@ int main () {
     //     default: cout << "E" << endl;
     // }
 
-    // Ternary Operator
+    // Ternary Operator, penulisan percabangan yang lebih ringkas
     // if (nilai % 2 == 0) {
     //     cout << "Genap sih" << endl;
     // } else {
