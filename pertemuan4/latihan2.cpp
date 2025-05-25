0eb7f1876871649d5a10b0e3b949a10ef02c2f2f
JefriAlBukhori200505 <jefrial@students.usu.ac.id>
2025-05-25 14:59:48 +0700

    pertemuan 4 : perulangan

--- pertemuan4/latihan2.cpp
+++ pertemuan4/latihan2.cpp
@@ -2,20 +2,20 @@
 using namespace std;
 
 int main(){
-    string kalimat;
-    int i;
+    string kalimat; // variabel untuk menyimpan kalimat
+    int i; // variabel untuk iterasi dalam perulangan
 
-    system("CLS");
+    system("CLS"); // membersihkan layar
 
-    cout << "Masukkan kalimat : "; getline(cin, kalimat);
+    cout << "Masukkan kalimat : "; getline(cin, kalimat); // mengambil satu baris termasuk spasi
 
-    for (i = 0; i < kalimat.length(); i++) {
-        if (kalimat[i] == 'a' || kalimat[i] == 'e' || kalimat[i] == 'i' || kalimat[i] == 'o' || kalimat[i] == 'u')
+    for (i = 0; i < kalimat.length(); i++) { // perulangan untuk memeriksa setiap karakter dalam string
+        if (kalimat[i] == 'a' || kalimat[i] == 'e' || kalimat[i] == 'i' || kalimat[i] == 'o' || kalimat[i] == 'u') // jika karakter adalah huruf vokal kecil (a, e, i, o, u)
         {
-            kalimat[i] = toupper(kalimat[i]);
+            kalimat[i] = toupper(kalimat[i]); // mengubah huruf menjadi huruf besar
         }
     }
 
     cout << "Kalimat dalam huruf kapital : "  << kalimat << endl;
-    return 0;
+    return 0; // mengakhiri program
 }
