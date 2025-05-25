79be6c40ca2fff50519c92a627d5a5da6b554042
JefriAlBukhori200505 <jefrial@students.usu.ac.id>
2025-05-25 13:04:06 +0700

    pertemuan 2 : penggunaan operator

--- pertemuan2/latihan.cpp
+++ pertemuan2/latihan.cpp
@@ -1,19 +1,19 @@
-#include <iostream>
-#include <cmath>
+#include <iostream> // header untuk cout/cin
+#include <cmath> // header untuk fungsi matematika
 
-using namespace std;
+using namespace std; // agar tidak perlu membuat std:: sebelum cout/cin
 
 int main() {
-    const double PI = 3.14;
-    double radius, volume, permukaan;
+    const double PI = 3.14; // konstanta PI, digunakan dalam perhitungan bola
+    double radius, volume, permukaan; // variabel bertipe double untuk jari-jari, volume, dan luas permukaan
 
     cout << "Masukkan jari-jari bola: ";
     cin >> radius;
 
-    volume = (4.0 / 3.0) * PI * pow(radius, 3);
-    permukaan = 4 * PI * pow(radius, 2);
+    volume = (4.0 / 3.0) * PI * pow(radius, 3); // Menghitung volume bola 
+    permukaan = 4 * PI * pow(radius, 2); // Menghitung luas permukaan bola 
 
     cout << "Volume bola: " << volume << endl;
     cout << "Luas permukaan bola: " << permukaan << endl;
-    return 0;
+    return 0; // mengembalikan nilai 0 sebagai tanda program selesai
 }
