7ce8867da4bb87bb668f4f4ee92cd8ceb5f78f00
JefriAlBukhori200505 <jefrial@students.usu.ac.id>
2025-05-25 12:53:09 +0700

    pertemuan 1 : pengenalan & operasi input/output

--- pertemuan1/belajarC.cpp
+++ pertemuan1/belajarC.cpp
@@ -1,27 +1,27 @@
 #include <stdio.h> // header untuk C
-#include <conio.h>
+#include <conio.h> // header untuk getch(), digunakan untuk input karakter tanpa menunggu enter
 
 int main() {
-    char nama[50];
-    int nim;
-    char kom[2];
-    float ip;
+    char nama[50]; // variabel array untuk menyimpan nama (maksimal 50)
+    int nim;  // variabel untuk menyimpan nim
+    char kom[2]; // variabel array untuk menyimpan kom (maksimal 2)
+    float ip; // variabel untuk menyimpan ip
 
     printf("Hello World\n"); // fungsi keluaran pada C
 
     printf("Masukkan nama : ");
-    gets(nama); // get string
+    gets(nama); // Membaca input string dan menyimpannya di variabel nama
 
     printf("Masukkan nim : ");
-    scanf("%d", &nim); // fungsi masukan pada C
+    scanf("%d", &nim); // Membaca input integer dan menyimpannya ke variabel nim
 
-    getchar();
+    getchar(); // // Membersihkan karakter newline (\n) dari input sebelumnya agar tidak mengganggu gets selanjutnya
 
     printf("Masukkan kom : ");
-    gets(kom); 
+    gets(kom); // Membaca input string untuk KOM
 
     printf("Masukkan ip : ");
-    scanf("%f", &ip); 
+    scanf("%f", &ip); // Membaca input float dan menyimpannya ke variabel ip
 
     /* Untuk Output */
     printf("Nama : ");
@@ -30,9 +30,9 @@ int main() {
     printf("NIM : %d\n", nim);
 
     printf("KOM : ");
-    puts(kom); 
-    printf("IP : %f\n", ip);
+    puts(kom); // put string, digunakan untuk string dalam bentuk array char
+    printf("IP : %f\n", ip); // Menampilkan isi dari variabel ip dalam float
 
-    printf("press any button to continue...");
-    getch();
+    printf("press any button to continue..."); // Menampilkan pesan ke pengguna
+    getch(); // Menunggu input satu karakter sebelum menutup program, tanpa perlu tekan Enter
 }
