459f5cd642d1c5a1138b61a59636d5601aeccea4
JefriAlBukhori200505 <jefrial@students.usu.ac.id>
2025-05-25 13:12:35 +0700

    pertemuan 2 : penggunaan operator

--- pertemuan2/luaspanjang.cpp
+++ pertemuan2/luaspanjang.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // header untuk cout/cin
+using namespace std; // agar tidak perlu membuat std:: sebelum cout/cin
 
 int main () {
 
-    float luas, p, l;
+    float luas, p, l; // variabel float untuk menampilkan p untuk panjang, l untuk lebar dan luas
 
     cout << "Masukkan panjang : ";
     cin >> p;
@@ -11,8 +11,8 @@ int main () {
     cout << "Masukkan lebar : ";
     cin >> l;
 
-    luas = p * l;
+    luas = p * l; // menghitung luas
     cout << "Luas = " << luas << endl;
 
-    return 0;
+    return 0; // mengembalikan nilai 0 sebagai tanda program selesai
 }
