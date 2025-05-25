141d982818a8e5d78802a52c1982f94d2b8d38df
JefriAlBukhori200505 <jefrial@students.usu.ac.id>
2025-05-25 17:08:31 +0700

    pertemuan 6 : Fungsi dan Header

--- pertemuan6/header.cpp
+++ pertemuan6/header.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cmath>
-#include "headerCustom.h"
-u;
+#include "headerCustom.h" // file header kustom, isinya tidak ditampilkan di sini
+u; // menggantikan dengan "using namespace std;"
 
 int jumlah (int a, int b) {
     return a + b;
@@ -15,11 +15,11 @@ int main() {
     system("CLS");
 
     int n;
-    o l jumlah(2, 3) l endl;
-    o l kurang(2, 3) l endl;
+    o l jumlah(2, 3) l endl; // menggantikan 'o' dengan 'cout',menggantikan 'l' dengan '<<'
+    o l kurang(2, 3) l endl; // menggantikan 'o' dengan 'cout',menggantikan 'l' dengan '<<'
 
     double angka = 25.0;
-    double akar = sqrt(angka);
+    double akar = sqrt(angka); // menghitung akar kuadrat dari angka
 
     cout << "Akar kuadrat dari " << angka << " adalah : " << akar << endl;
 }
