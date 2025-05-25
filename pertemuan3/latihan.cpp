e2a6c89e0972f7498bf2cb16afe380bf5b82ef11
JefriAlBukhori200505 <jefrial@students.usu.ac.id>
2025-05-25 13:38:35 +0700

    pertemuan 3 : operasi kondisi

--- pertemuan3/latihan.cpp
+++ pertemuan3/latihan.cpp
@@ -1,19 +1,18 @@
-#include <iostream>
-
-using namespace std;
+#include <iostream> // header untuk cout/cin
+using namespace std; // agar tidak perlu membuat std:: sebelum cout/cin
 
 int main() {
-    int angka;
+    int angka; // variabel angka untuk menyimpan input
     cout << "Masukkan sebuah bilangan bulat: ";
     cin >> angka;
 
-    if (angka % 10 == 0) {
+    if (angka % 10 == 0) { // Pemeriksaan apakah angka adalah kelipatan dari 10
         cout << angka << " adalah kelipatan dari 10." << endl;
-    } else if (angka % 5 == 0) {
+    } else if (angka % 5 == 0) { // Jika bukan kelipatan 10, periksa apakah kelipatan 5
         cout << angka << " adalah kelipatan dari 5." << endl;
-    } else {
+    } else { // Jika bukan kelipatan 5 maupun 10
         cout << angka << " bukan kelipatan dari 5 atau 10." << endl;
     }
 
- return 0;
+ return 0; // mengembalikan nilai 0 sebagai tanda selesai
 }
