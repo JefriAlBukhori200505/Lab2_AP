09aa571e3cd2daf6eb01d936d5e71c5704535fca
JefriAlBukhori200505 <jefrial@students.usu.ac.id>
2025-05-25 17:29:47 +0700

    pertemuan 8 : Struct & Class

--- pertemuan8/aksesstruct.cpp
+++ pertemuan8/aksesstruct.cpp
@@ -2,25 +2,25 @@
 #include <string>
 using namespace std;
 
-struct mahasiswa{
-    string nama;
-    int umur;
-    float ipk;
+struct mahasiswa{ // membuat struktur data 'mahasiswa'
+    string nama; // menyimpan nama mahasiswa
+    int umur; // menyimpan umur mahasiswa
+    float ipk; // menyimpan IPK mahasiswa
 };
 
 int main() {
 
-    mahasiswa mhs1;
+    mahasiswa mhs1; // membuat variabel struct bertipe 'mahasiswa'
 
-    mhs1.nama = "Jefri";
-    mhs1.umur = 20;
-    mhs1.ipk = 4.00;
+    mhs1.nama = "Jefri"; // mengisi nama
+    mhs1.umur = 20; // mengisi umur
+    mhs1.ipk = 4.00; // mengisi ipk
 
     // cout << " nama = " << mhs1.nama << endl;
     // cout << " umur = " << mhs1.umur << endl;
     // cout << " ipk = " << mhs1.ipk << endl;
 
-    mahasiswa *ptrmhs = &mhs1;
+    mahasiswa *ptrmhs = &mhs1; // membuat pointer ke struct mahasiswa dan menunjuk ke mhs1
     cout << " nama = " << ptrmhs->nama << endl;
     cout << " umur = " << ptrmhs->umur << endl;
     cout << " ipk = " << ptrmhs->ipk << endl;
